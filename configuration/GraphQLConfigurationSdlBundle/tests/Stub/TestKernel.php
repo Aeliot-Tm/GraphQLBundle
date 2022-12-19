58ef7236c9970701dc36e94c71309a86793e2c15
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-12-19 16:05:06 +0300

    Update PHPStan config

---
+++ configuration/GraphQLConfigurationSdlBundle/tests/Stub/TestKernel.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Overblog\GraphQLConfigurationSdlBundle\Tests\Stub;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as SymfonyKernel;
+use function dirname;
+
+final class TestKernel extends SymfonyKernel
+{
+    use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir(): string
+    {
+        return dirname($this->getProjectDir()).'/var/cache/'.$this->environment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir(): string
+    {
+        return dirname($this->getProjectDir()).'/var/log';
+    }
+
+    protected function getContainerClass(): string
+    {
+        return parent::getContainerClass().'Tmp';
+    }
+}
---
+++ configuration/GraphQLConfigurationSdlBundle/tests/config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Overblog\GraphQLConfigurationSdlBundle\GraphQLConfigurationSdlBundle::class => ['all' => true],
+];
